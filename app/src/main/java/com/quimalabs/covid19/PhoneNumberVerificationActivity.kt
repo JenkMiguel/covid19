5ec964c4cfe4e16ff2c1d6e5cbef61dc28a122cc
Miguel Quispe <anvariel@hotmail.com>
2020-03-18 00:28:28 -0500

    Phone Number Auth
    Se implemento firebase sdk para autentificacion de celular

---
+++ app/src/main/java/com/quimalabs/covid19/PhoneNumberVerificationActivity.kt
@@ -0,0 +1,12 @@
+package com.quimalabs.covid19
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PhoneNumberVerificationActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_phone_number_verification)
+    }
+}
